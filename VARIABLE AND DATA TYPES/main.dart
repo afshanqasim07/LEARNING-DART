c15a6f2179c3bc5f6c59979748a683b089bcb622
afshanqasim07 <afshaanqasim07>
2025-02-20 02:44:23 -0800

    data type ,variable done

---
+++ VARIABLE AND DATA TYPES/main.dart
@@ -0,0 +1,21 @@
+// Varibales
+// we have two variables in dart var and final we can assign var value two times but final remains constant.
+void main() {
+  var name = "afshan";
+  print(name);
+  name = "ayesha";
+  print(name);
+  final age = 19;
+  print(age);
+  // data types
+  // 1.int(number 12) 2.double(decimal no 34.11)
+  // 3.boolean (true/false) 4.string('afshan')
+  int price = 50000;
+  print(price);
+  double cost = 20.11;
+  print(cost);
+  String item = "pencil";
+  print(item);
+  bool is_login = true;
+  print(is_login);
+}
