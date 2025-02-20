e4b522c58ff04e686184798a8fcf9afeb58e6ecd
afshanqasim07 <afshaanqasim07>
2025-02-20 02:07:44 -0800

    intro done

---
+++ INTRO TO DART/main.dart
@@ -0,0 +1,5 @@
+// dart is the dynamic programming langauge ressemble to javascript everycode of our dart language should be in main function
+
+void main() {
+  print("hello world");
+}
---
+++ OPERATORS/main.dart
@@ -0,0 +1,35 @@
+// we have so many operator in dart languge
+// AIRTHEMATIC OPERATOR
+//  1.addition 2.subtraction 3.multiply 4.devide 
+// 5.modulus
+// ASSIGMENT OPERATOR 
+
+main void(){
+var sumone = 50;
+var sumtwo = 25;
+print(sumone+sumtwo);
+print(sumone-sumtwo);
+print(sumone*sumtwo);
+print(sumone/sumtwo);
+print(sumone%sumtwo);
+// increment decrement 
+var add = 5;
+print(add++);
+print(add--);
+// assigment operator
+var price1 = 20;
+print(price1+=5);
+print(price1-=5);
+print(price1*=5);
+print(price1/=5);
+//  operators 
+// true/false
+var a = 70;
+var b = 50;
+print(a==b);
+print(a!=b);
+print(a>b);
+print(a<b);
+print(a>=b);
+print(a<=b);
+}
