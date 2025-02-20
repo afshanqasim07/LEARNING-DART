4cd6524d1021943887a804f71b070a903d01530d
afshanqasim07 <afshaanqasim07>
2025-02-20 06:14:09 -0800

    done strings

---
+++ CONDITIONAL STATEMENTS/main.dart
@@ -0,0 +1,11 @@
+void main() {
+  // CONDITIONAL STAMENETS AND TERNARY OPERATOR
+  // conditional statments are the statements which works on condition ternary operator are the shortcut of if else
+  var a = 60;
+  var b = 50;
+  if (a > b) {
+    print("A is greater then b");
+  } else {
+    print("B is greater then A");
+  }
+}
---
+++ STRINGS/main.dart
@@ -0,0 +1,30 @@
+void main() {
+  // strings
+  // we can use use string for any word/senetence /statemnet like in the ""in this .
+  var name = "afshan";
+  var fathername = "qasim";
+  final age = 19;
+  // print("my name is $name and father name is $fathername and age is $age");
+  // if we add '''here ''' like this strings then it create the gap with the way we do
+  // example
+  // print(''' hello
+  // my name is $name
+  // my fathername is $fathername
+  // and
+  // age is $age''');
+  // methods for strings
+  // length /index
+  // print(name.length);
+  // print(name[2]);
+  // uppercase/lowercase
+  // print(name.toLowerCase());
+  // print(name.toUpperCase());
+  // split (this method spilit the whole word by ('')and change it by (*))
+  // print(name.split('*'));
+  // replace (this will replace your string with some methods)
+  // print(name.replaceAll(name, 'replace'));
+  // print(name.replaceFirst('f', 'b'));
+  // contain (this checks that word exist or not then give you true r false )
+  print(name.contains('j'));
+  print(name.contains('h'));
+}
