73242192e17bdc7c5a450dbc8ed59663a5c63b54
afshanqasim07 <afshaanqasim07>
2025-02-20 06:34:58 -0800

    conitional statments done

--- CONDITIONAL STATEMENTS/main.dart
+++ CONDITIONAL STATEMENTS/main.dart
@@ -1,11 +1,24 @@
 void main() {
   // CONDITIONAL STAMENETS AND TERNARY OPERATOR
   // conditional statments are the statements which works on condition ternary operator are the shortcut of if else
-  var a = 60;
-  var b = 50;
-  if (a > b) {
-    print("A is greater then b");
-  } else {
-    print("B is greater then A");
-  }
+  // var a = 60;
+  // var b = 50;
+  // if ((a > b) ){
+  //   print("A is greater then b");
+  // } else {
+  //   print("B is greater then A");
+  // }
+  // nested if else statement
+  // var marks = 400;
+  // if (marks == 400) {
+  //   print('marks is greater equal to 400');
+  // } else if (marks == 350) {
+  //   print('marks is greater equal to 350');
+  // } else {
+  //   print("doesnt match the condition");
+  // }
+  // ternary operator
+  var age = 15;
+  var allowedage = (age >= 18) ? 'you can drive' : 'you cant drive';
+  print(allowedage);
 }
