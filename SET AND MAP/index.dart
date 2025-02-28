8c25149c7380e8d235b5f94f194a522771071742
afshanqasim07 <afshaanqasim07>
2025-02-28 07:39:17 -0800

    map methods done

--- SET AND MAP/index.dart
+++ SET AND MAP/index.dart
@@ -11,17 +11,34 @@ void main() {
     "followers": 250,
     "following": 100,
   };
-  print(userid);
-  print(userid.keys);
-  print(userid.values);
-  print(userid['followers']);
+  // print(userid);
+  // print(userid.keys);
+  // print(userid.values);
+  // print(userid['followers']);
   // . Map Constructor
 // Agar aap explicitly Map banani chahein, toh Map<K, V>() constructor use kar sakte hain.
 // ye jo hai empty map bnata ha jo hmri keysvalues accept krega Later on;
+// and thats how we add the keyspairs in map ;
   var abouther = Map<String, dynamic>();
   abouther['name'] = 'ayesha';
   abouther['age'] = 19;
-  print(abouther);
-  print(abouther.keys);
-  print(abouther.values);
+  // print(abouther);
+  // print(abouther.keys);
+  // print(abouther.values);
+  // agr koi value access krne k koshish kre wo exist nh krt tou null show huta ha
+  // check length
+  print(userid.length);
+  // checking if key exists true/false
+  print(abouther.containsKey('age'));
+  // checking if value exists true/false
+  print(abouther.containsValue(15));
+  // remove key pair
+  userid.remove('following');
+  print(userid);
+  // 5. Looping Through a Map
+// Agar aap poore map ke elements ko iterate karna chahein, toh .forEach() ya for-in loop use kar sakte hain.
+  userid.forEach((key, value) {
+    print('$key : $value' );
+  });
+  
 }
